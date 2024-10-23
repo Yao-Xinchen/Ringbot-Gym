3217400787b018e47bd51a0a58def7a4e0ce18c7
Yao-Xinchen <yao29@illinois.edu>
2024-10-23 18:26:27 -0400

    define the step for ringbot

--- ringbot_gym/envs/ringbot/ringbot.py
+++ ringbot_gym/envs/ringbot/ringbot.py
@@ -1,4 +1,4 @@
-from ringbot_gym import GYM_ROOT_DIR,envs
+from ringbot_gym import GYM_ROOT_DIR, envs
 from time import time
 from warnings import WarningMessage
 import numpy as np
@@ -21,5 +21,86 @@ from ringbot_gym.utils.math import (
 from ringbot_gym.utils.helpers import class_to_dict
 from .ringbot_config import RingbotCfg, RingbotCfgPPO
 
+
 class Ringbot(LeggedRobot):
-    pass
+    def __init__(self, cfg: RingbotCfg, sim_params, physics_engine, sim_device, headless):
+        self.cfg = cfg
+        self.sim_params = sim_params
+        self.height_samples = None
+        self.debug_viz = False
+        self.init_done = False
+        self._parse_cfg(cfg)
+        super().__init__(cfg, sim_params, physics_engine, sim_device, headless)
+        self.pi = torch.tensor(np.pi, device=self.device)
+
+        if not self.headless:
+            self.set_camera(self.cfg.viewer.pos, self.cfg.viewer.lookat)
+        self._init_buffers()
+        self._prepare_reward_function()
+        self.init_done = True
+
+    def step(self, actions):
+        clip_actions = self.cfg.normalization.clip_actions
+        self.actions = torch.clip(actions, -clip_actions, clip_actions).to(self.device)
+        self.render()
+        # self.pre_physics_step()
+
+        # physics step
+        for _ in range(self.cfg.control.decimation):
+            self.envs_steps_buf += 1  # TODO: define this
+            self.action_ringbuf = torch.cat(
+                (self.actions.unsqueeze(1), self.action_ringbuf[:, :-1]),
+                dim=1
+            )
+            self.torques = self._compute_torques(
+                self.action_ringbuf[torch.arange(self.num_envs), self.action_delay_idx, :]
+            )
+            self.gym.set_dof_actuation_force_tensor(
+                self.sim, gymtorch.unwrap_tensor(self.torques)
+            )
+            # TODO: other physics steps
+
+        self.post_physics_step()
+
+        # observation
+        clip_obs = self.cfg.normalization.clip_observations
+        self.obs_buf = torch.clip(self.obs_buf, -clip_obs, clip_obs)
+        if self.privileged_obs_buf is not None:
+            self.privileged_obs_buf = torch.clip(self.privileged_obs_buf, -clip_obs, clip_obs)
+        return (
+            self.obs_buf,
+            self.privileged_obs_buf,  # TODO: check this
+            self.rew_buf,
+            self.reset_buf,
+            self.extras,
+            self.obs_history,  # TODO: define this
+        )
+
+    def _compute_torques(self, actions):
+        num_torques = self.cfg.env.num_actions  # TODO: define this
+        torques = torch.zeros(
+            self.num_envs, num_torques, device=self.device
+        )
+        # TODO: define this
+        return torques
+
+    def post_physics_step(self):
+        pass
+
+    def _init_buffers(self):
+        super()._init_buffers()
+        delay_max = np.int64(
+            np.ceil(self.cfg.domain_rand.delay_ms_range[1] / 1000 / self.sim_params.dt)
+        )
+        self.action_ringbuf = torch.zeros(
+            (self.num_envs, delay_max, self.cfg.env.num_actions),
+            dtype=torch.float,
+            device=self.device,
+            requires_grad=False,
+        )
+        self.action_delay_idx = torch.zeros(
+            self.num_envs,
+            dtype=torch.long,
+            device=self.device,
+            requires_grad=False,
+        )  # TODO: update this
--- ringbot_gym/envs/ringbot/ringbot_config.py
+++ ringbot_gym/envs/ringbot/ringbot_config.py
@@ -4,7 +4,15 @@ from ringbot_gym.envs.base.legged_robot_config import (
 )
 
 class RingbotCfg(LeggedRobotCfg):
-    pass
+    class domain_rand:
+        randomize_friction = True
+        friction_range = [0.5, 1.25]
+        randomize_base_mass = False
+        added_mass_range = [-1., 1.]
+        push_robots = True
+        push_interval_s = 15
+        max_push_vel_xy = 1.
+        delay_ms_range = [0, 10]
 
 class RingbotCfgPPO(LeggedRobotCfgPPO):
     pass
